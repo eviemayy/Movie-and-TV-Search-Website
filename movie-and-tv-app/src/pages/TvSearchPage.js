f911f2ec62e2ca168a5fb40076d2691a6d81393c
QuinnBGC <61990657+QuinnBGC@users.noreply.github.com>
2021-03-11 22:49:26 -0800

    Add files via upload

--- movie-and-tv-app/src/pages/TvSearchPage.js
+++ movie-and-tv-app/src/pages/TvSearchPage.js
@@ -1,8 +1,11 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
+ /**@jsxImportSource @emotion/react */
 
-import { jsx } from '@emotion/react';
 import styled from '@emotion/styled/macro';
+import { css } from '@emotion/react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { getTVShow } from '../components/getTVShow';
 
 const Page = styled.div`
     background-color: #2f445e;
@@ -11,13 +14,105 @@ const Page = styled.div`
     min-height: calc(100vh - 70px);
 `;
 
-function TvSearchPage() {
+function Button(props) {
+    //console.log("== button props:", props);
+    const styles = css`
+        type: submit;
+      color: ${props.secondary ? 'royalblue' : 'snow'};
+      background-color: ${props.secondary ? 'whitesmoke' : 'royalblue'};
+      border: 2px solid royalblue;
+      padding: 10px;
+      margin: 5px;
+      margin-left: 15px;
+      cursor: pointer;
+      &:hover {
+        background-color: dodgerblue;
+      }
+    `;
+    return <button css={styles}>{props.children}</button>;
+}
+
+function InputDiv(props) {
+    //console.log("== inputDiv props:", props);
+    const styles = css`
+    margin: auto;
+    width: 250px;
+      color: ${props.secondary ? 'royalblue' : 'snow'};
+      background-color: 'whitesmoke';
+      border: 2px solid grey;
+      padding: 10px;
+      
+      cursor: pointer;
+      
+    `;
+    return <div css={styles}>{props.children}</div>;
+}
+
+function TVSearchPage({query}) {
+    const [inputQuery, setinputQuery] = useState(query || "");
+    const [TVShow, setTVShow] = useState([]);
+    const [TVquery, setTVquery] = useState("");
+
+    const history = useHistory();
+    useEffect(() => {
+        let ignore = false;
+        const findTVShow = async (e) => {
+            let data = {};
+            try {
+                const res = await getTVShow(query);
+                data = await res;
+            }catch (e) {
+                if (e instanceof DOMException) {
+                console.log("HTTP request aborted");
+                } else {
+                throw e;
+                }
+            }
+            console.log("== data.Title:", data.Title);
+            //setTVShow(data);
+            if (!ignore) {
+                console.log("== inputQuery:", query);
+                console.log("== data:", data);
+                setTVShow(data.list || []);
+                setTVquery(data.array || []);
+
+            }
+        }
+        findTVShow();
+        return () => {
+            //controller.abort();
+            ignore = true;
+        };
+
+    }, [ query ]);
+
     return (
         <Page>
-            <h1>TvSearchPage</h1>
+        <h1>TvSearchPage</h1>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            history.push(`?q=${inputQuery}`);
+            }}>
+            <InputDiv>
+            <input type="text" value={inputQuery}onChange={(e) => setinputQuery(e.target.value)}/>
+            <Button>Search</Button>
+            </InputDiv>
+        </form>
+        <h1>Results for {query}</h1>
+        <h1>Results for {TVquery.Title}</h1>
+        
+        
+        {TVShow.map(show => (
+            <div>
+                        <p> {show.Title} </p>
+                        
+                        
+            </div>
+                    
+                    
+        ))}
         </Page>
-
     );
 }
 
-export default TvSearchPage;
+export default TVSearchPage;
