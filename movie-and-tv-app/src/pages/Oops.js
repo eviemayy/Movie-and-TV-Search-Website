d73a9ccaed11bafe824f44e038605436044c47f3
eviemayy <43556792+eviemayy@users.noreply.github.com>
2021-03-18 10:19:52 -0700

    404 page and fix image sizing

--- movie-and-tv-app/src/pages/Oops.js
+++ movie-and-tv-app/src/pages/Oops.js
@@ -1,8 +1,39 @@
-import React from 'react';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
+import { jsx } from '@emotion/react';
+import styled from '@emotion/styled/macro';
+
+const Page = styled.div`
+    background-color: #2f445e;
+    padding: 10px;
+    margin: 0px;
+    min-height: calc(100vh - 70px);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    h1{
+        align-self: center;
+        font-size: 50px;
+    }
+
+    img{
+        align-self: center;
+        font-size: 70px;
+        height: 400px;
+        width: 600px;
+        padding: 20px;
+    }
+`;
 
 function Oops() {
     return (
-        <h1>404 Page</h1>
+        <Page>
+
+            <img src="https://media.giphy.com/media/26xBzXOrvZ8y5Wro4/giphy.gif" alt="404 page"></img>
+            <h1>Oh no! Page not found</h1>
+        </Page>
     );
 }
 
